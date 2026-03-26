f39c42f8946dcd592762ab4d987f86d09ece3de9
AmirAbidi7 <abidiamir7@gmail.com>
2026-03-26 19:34:29 +0100

    Added createdAt fields to track creation dates

--- backend/internal/models/pdf.go
+++ backend/internal/models/pdf.go
@@ -1,16 +1,20 @@
 package models
 
+import "time"
+
 type Pdf struct {
-	ID       uint   `json:"id"`
-	Size     uint   `json:"size"`
-	Pages    uint   `json:"pages"`
-	Checksum string `json:"-"`
-	Users    []User `gorm:"many2many:user_pdf;" json:"users"`
+	ID        uint   `json:"id"`
+	Size      uint   `json:"size"`
+	Pages     uint   `json:"pages"`
+	Checksum  string `json:"-"`
+	CreatedAt time.Time
+	Users     []User `gorm:"many2many:user_pdf;" json:"users"`
 }
 
 type UserPdf struct {
 	PdfID       uint   `gorm:"primaryKey" json:"pdf_id"`
-	Title       string `json:"title"`
 	UserID      uint   `gorm:"primaryKey" json:"user_id"`
+	Title       string `json:"title"`
 	CurrentPage uint   `json:"current_page" gorm:"default:1"`
+	CreatedAt   time.Time
 }
